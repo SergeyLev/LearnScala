7dbd282326534461baca9cbda3c560c251ff0e08
SergeyLev <36793484+SergeyLev@users.noreply.github.com>
2019-01-13 01:20:50 +0200

    Generics + Anonymous Classes

---
+++ untitled1/src/lectures/part2/oop/AnonymousClasses.scala
@@ -0,0 +1,21 @@
+package lectures.part2.oop
+
+object AnonymousClasses extends App{
+
+  trait Animal{
+    def eat: Unit
+  }
+
+  // anonymous class
+  val funnyAnimal: Animal = new Animal {
+    override def eat: Unit = println("ahahahah")
+  }
+
+  println(funnyAnimal.getClass)
+
+  class Person(name: String){
+    def sayHi: Unit = println(s"Hi, $name!")
+  }
+
+  val jim = new Person("Jim")
+}
