c7b96bb70a0e6620a5201172babed22a3068e140
SergeyLev <36793484+SergeyLev@users.noreply.github.com>
2019-01-13 16:40:36 +0200

    bash initial

---
+++ demo_filesystem/src/com/scala/oop/files/Directory.scala
@@ -0,0 +1,35 @@
+package com.scala.oop.files
+
+class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
+  extends DirEntry(parentPath, name) {
+
+  def hasEntry(name: String): Boolean = ???
+
+  def getAllFoldersInPath: List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList
+
+  def findDescendant(path: List[String]): Directory = ???
+
+  def addEntry(newEntry: DirEntry): Directory = ???
+
+  def findEntry(entryName: String): DirEntry = {
+    def findEntryHelper(name: String, contentList: List[DirEntry]): DirEntry =
+      if()
+  }
+
+  def replaceEntry(entryName: String, newEntry: DirEntry): Directory = ???
+
+  def asDirectory: Directory = this
+}
+
+
+object Directory{
+  val SEPARATOR = "/"
+  val ROOT_PATH = "/"
+
+
+  def ROOT: Directory = Directory.empty("", "")
+
+  def empty(parentPath: String, name: String): Directory =
+   new Directory(parentPath, name, List())
+}
