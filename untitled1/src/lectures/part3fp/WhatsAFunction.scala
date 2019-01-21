83b1856f82aa13ced007d3ff45f177b5d3ecb1d2
SergeyLev <36793484+SergeyLev@users.noreply.github.com>
2019-01-21 17:43:33 +0200

    FP

--- untitled1/src/lectures/part3fp/WhatsAFunction.scala
+++ untitled1/src/lectures/part3fp/WhatsAFunction.scala
@@ -2,7 +2,25 @@ package lectures.part3fp
 
 object WhatsAFunction extends App{
 
-  // use functions as first class elements
+  val doubler = new MyFunction[Int, Int] {
+    override def apply(element: Int): Int =
+      element * 2
+  }
 
+  println(doubler(2))
 
+  val stringToIntConverter = new Function[String, Int] {
+    override def apply(v1: String): Int =
+      v1.toInt
+  }
+
+  println(stringToIntConverter("3") + 4)
+
+  val adder: ((Int, Int) => Int) = new Function2[Int, Int, Int] {
+    override def apply(v1: Int, v2: Int): Int = v1 + v2
+  }
+}
+
+trait MyFunction[A, B]{
+  def apply(element: A): B
 }
